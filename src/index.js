6e5ca32ec7c52f832985ba08aae7fe96fefe004f
Levy Wechuli <levy.wechuli@student.moringaschool.com>
2024-10-06 22:36:47 +0300

    initialcommit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ const form = document.getElementById("create-task-form");
+
+ const taskList = document.getElementById("tasks");
+
+ const deleate = document.getElementById("btn");
+
+ form.addEventListener("submit", (event) =>{
+  event.preventDefault();
+
+deleate.textContent = "Delete";
+deleate.className = "delete-btn";
+
+
+
+
+  const taskInput = document.getElementById("task Description");
+  const taskDescription = taskInput.value.trim();
+
+  if(taskDescription){
+    const listItems = document.createElement("li");
+    listItems.textContent = taskDescription;
+
+
+    taskList.appendChild(listItems);
+
+    taskInput.value = "";
+  }else{
+    alert("please enter a task description");
+  }
+ });
+
+
 });
